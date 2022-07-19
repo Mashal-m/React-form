710c19c07c59cd8e8bf16cd8ad368ac2ae0258b7
Mashal-m <mashal.malik@arbisoft.com>
2022-07-19 14:20:53 +0500

    fix: file name issue resolved

--- react-form/src/components/SignupForm.test.js
+++ react-form/src/components/SignupForm.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import SignupForm from "./SIgnupForm";
+import SignupForm from "./SignupForm";
 import { fireEvent } from "@testing-library/react";
 
 describe("SignupForm", () => {
